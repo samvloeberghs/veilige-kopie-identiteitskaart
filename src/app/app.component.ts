66ca6719c77f70eeed6ae96a0476b31d176b4c65
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2023-11-26 22:40:08 +0100

    chore(*): more fixes + sw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectorRef, Component, ElementRef, inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { ChangeDetectorRef, Component, effect, inject, OnDestroy, ViewChild } from '@angular/core';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -8,35 +8,65 @@ import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { NgIf } from '@angular/common';
 import { filter } from 'rxjs';
+import { UpdateService } from '../services/update.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
     standalone: true,
-    imports: [RouterModule, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, NgIf],
+    imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, NgIf],
     selector: 'vki-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnDestroy {
-    public mobileQuery: MediaQueryList;
+    readonly #changeDetectorRef = inject(ChangeDetectorRef);
+    readonly #media = inject(MediaMatcher);
+    readonly #router = inject(Router);
+    readonly #updateService = inject(UpdateService);
+    readonly #matSnackBar = inject(MatSnackBar);
+
+    #mobileQueryListener!: () => void;
+
+    public readonly currentVersionSignal = this.#updateService.currentVersionSignal;
+    public mobileQuery!: MediaQueryList;
 
     @ViewChild('sideNav')
     private readonly sideNav!: MatSidenav;
 
-    private _mobileQueryListener: () => void;
+    constructor() {
+        this.setupMobileQueryListener();
+        this.setupRouteListener();
+        this.setupUpdateListener();
+    }
 
-    constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, router: Router) {
-        this.mobileQuery = media.matchMedia('(max-width: 600px)');
-        this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-        this.mobileQuery.addListener(this._mobileQueryListener);
-        router.events.pipe(
+    public ngOnDestroy(): void {
+        this.mobileQuery.removeListener(this.#mobileQueryListener);
+    }
+
+    private setupMobileQueryListener(): void {
+        this.mobileQuery = this.#media.matchMedia('(max-width: 600px)');
+        this.#mobileQueryListener = () => this.#changeDetectorRef.detectChanges();
+        this.mobileQuery.addListener(this.#mobileQueryListener);
+    }
+
+    private setupRouteListener(): void {
+        this.#router.events.pipe(
             filter((event) => event instanceof NavigationEnd)
         ).subscribe((event) => {
             this.sideNav?.close();
         });
     }
 
-    public ngOnDestroy(): void {
-        this.mobileQuery.removeListener(this._mobileQueryListener);
+    private setupUpdateListener(): void {
+        effect(() => {
+            const newVersionAvailable = this.#updateService.newVersionAvailableSignal();
+            if (newVersionAvailable) {
+                const snackBarRef = this.#matSnackBar.open('New version available', 'Reload');
+                snackBarRef.onAction().subscribe(() => {
+                    document.location.reload();
+                });
+            }
+        });
     }
 
 }
---
+++ src/services/update.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, signal } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+
+@Injectable({providedIn: 'root'})
+export class UpdateService {
+
+    public readonly currentVersionSignal = signal('local-dev');
+    public readonly newVersionAvailableSignal = signal(false);
+
+    constructor(updates: SwUpdate) {
+        updates.versionUpdates.subscribe(evt => {
+            switch (evt.type) {
+                case 'VERSION_DETECTED':
+                    console.log(`Downloading new app version: ${evt.version.hash}`);
+                    break;
+                case 'VERSION_READY':
+                    console.log(`Current app version: ${evt.currentVersion.hash}`);
+                    console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
+                    this.currentVersionSignal.set(evt.currentVersion.hash);
+                    this.newVersionAvailableSignal.set(true);
+                    break;
+                case 'VERSION_INSTALLATION_FAILED':
+                    console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
+                    break;
+            }
+        });
+    }
+}
