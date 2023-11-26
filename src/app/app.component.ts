fefbcaf4fd2f97131e50e171b5d87062a9509470
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2023-11-26 18:41:29 +0100

    chore(*): improvement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,20 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { NgIf } from '@angular/common';
 
 @Component({
     standalone: true,
-    imports: [RouterModule, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule],
+    imports: [RouterModule, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, NgIf],
     selector: 'vki-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnDestroy {
     public mobileQuery: MediaQueryList;
+    public showScanPreviewFront = false;
+    public showScanPreviewBack = false;
+
     private _mobileQueryListener: () => void;
 
     constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
@@ -29,7 +33,46 @@ export class AppComponent implements OnDestroy {
     }
 
     public onFileSelected(event: Event, side: 'front' | 'back'): void {
-       console.log((event.target as HTMLInputElement).files, side);
+        console.log((event.target as HTMLInputElement).files, side);
+    }
+
+    public handleScan(side: 'front' | 'back', command: 'cancel' | 'preview'): void {
+        if (command === 'cancel') {
+            this.showScanPreviewBack = false;
+            this.showScanPreviewFront = false;
+        }
+
+        if (command === 'preview') {
+            if(side === 'front') {
+                this.showScanPreviewBack = false;
+                this.showScanPreviewFront = true;
+            }
+            if(side === 'back') {
+                this.showScanPreviewBack = true;
+                this.showScanPreviewFront = false;
+            }
+            this.showPreview(side);
+        }
+    }
+
+    private showPreview(side: 'front' | 'back'): void {
+        setTimeout(() => {
+            const player = document.getElementById(side) as HTMLVideoElement;
+            const constraints = {
+                video: {
+                    advanced: [{
+                        facingMode: 'environment',
+                        aspectRatio: 16 / 9,
+                        width: 1280,
+                        height: 720,
+                    }],
+                }
+            };
+
+            navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+                player.srcObject = stream;
+            });
+        });
     }
 
 }
