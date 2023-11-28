6af3bcfddcf23f6d5914a91d74cccb3bb6465d81
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2023-11-28 16:33:06 +0100

    chore(*): update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,22 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
     {
         path: '',
+        title: 'Startpagina - Veilige Kopie Identiteitskaart',
         loadComponent: () => import('../pages/home/home.component')
     },
     {
-        path: 'over-ons',
+        path: 'over-deze-applicatie',
+        title: 'Over deze applicatie - Veilige Kopie Identiteitskaart',
         loadComponent: () => import('../pages/about/about.component')
     },
     {
         path: 'kopie-maken',
+        title: 'Kopie maken - Veilige Kopie Identiteitskaart',
         loadComponent: () => import('../pages/make-copy/make-copy.component')
+    },
+    {
+        path: 'disclaimer-privacy-policy',
+        title: 'Disclaimer en privacy policy - Veilige Kopie Identiteitskaart',
+        loadComponent: () => import('../pages/disclaimer-privacy-policy/disclaimer-privacy-policy.component')
     },
 ];
